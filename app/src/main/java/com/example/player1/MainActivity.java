d425217dfd51912b55ca61a6e0e5945aff2f076e
itzik <itzikc2.ic@gmail.com>
2020-03-31 19:59:10 +0300

    add logic to radio button with log

--- app/src/main/java/com/example/player1/MainActivity.java
+++ app/src/main/java/com/example/player1/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.player1;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,14 +16,26 @@ public class MainActivity extends AppCompatActivity {
     private SurfaceView surfaceViewLay;
     private SurfaceHolder surfaceHolder;
     private ImageView play;
+    private ImageView stopButton;
+
     private RadioButton radioButtonTransport;
     private RadioButton radioButtonSplit;
     private RadioButton radioButtonRTSP;
-    private LinearLayout linearLayoutTransport;
+
+    private RadioButton radioButtonUdp;
+    private RadioButton radioButtonTcp;
+
+    private RadioButton radioButtonUnicast;
+    private RadioButton radioButtonMulticast;
+
+    private EditText portInputText;
+    private EditText sdpInputText;
+    private EditText ipInputText;
+
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
-
     }
 
     @Override
@@ -32,41 +45,115 @@ public class MainActivity extends AppCompatActivity {
         radioButtonTransport = findViewById(R.id.radioButtonTransport);
         radioButtonSplit = findViewById(R.id.radioButtonSplit);
         radioButtonRTSP = findViewById(R.id.radioButtonRTSP);
-        linearLayoutTransport  = findViewById(R.id.LinearLayoutTransport);
 
+        radioButtonUdp = findViewById(R.id.radioButtonUdp);
+        radioButtonTcp = findViewById(R.id.radioButtonTcp);
+        radioButtonUnicast = findViewById(R.id.radioButtonUnicast);
+        radioButtonMulticast = findViewById(R.id.radioButtonMulticast);
+
+
+        stopButton = findViewById(R.id.stopButton);
         surfaceViewLay = findViewById(R.id.surface_view);
         surfaceHolder = surfaceViewLay.getHolder();
 
-        radioButtonTransport.setOnClickListener(new View.OnClickListener() {
+        // all the input is in the this param
 
+        portInputText = findViewById(R.id.portInputText);
+        sdpInputText = findViewById(R.id.sdpInputText);
+        ipInputText = findViewById(R.id.ipInputText);
+
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //selected the radioButtonTransport
+                //stop button press
+                Log.v("stop_button", "stop button press");
 
-                linearLayoutTransport.setVisibility(View.VISIBLE);
-                //pgsBar.setVisibility(View.GONE);
             }
 
         });
+
+        radioButtonTransport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //selected the radioButtonTransport
+                radioButtonSplit.setChecked(false);
+                radioButtonRTSP.setChecked(false);
+                //stop button press
+                Log.v("radioButtonTransport", "radio Button Transport");
+            }
+        });
+
         radioButtonSplit.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                //selected the radioButtonSplit
-            }
+                radioButtonTransport.setChecked(false);
+                radioButtonRTSP.setChecked(false);
+                Log.v("radioButtonSplit", "radio Button Split");
 
+            }
         });
+
         radioButtonRTSP.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 //selected the radioButtonRTSP
-            }
+                radioButtonTransport.setChecked(false);
+                radioButtonSplit.setChecked(false);
+                Log.v("radioButtonRTSP", "radio Button RTSP");
 
+            }
+        });
+
+        radioButtonUdp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //stop button press
+                Log.v("radioButtonUdp", "radio Button Udp");
+                radioButtonTcp.setChecked(false);
+                radioButtonUnicast.setChecked(false);
+                radioButtonMulticast.setChecked(false);
+            }
+        });
+
+        radioButtonTcp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //stop button press
+                Log.v("radioButtonTcp", "radio Button Tcp");
+                radioButtonUdp.setChecked(false);
+                radioButtonUnicast.setChecked(false);
+                radioButtonMulticast.setChecked(false);
+            }
+        });
+
+        radioButtonUnicast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //stop button press
+                Log.v("radioButtonUnicast", "radio Button Unicast");
+                radioButtonUdp.setChecked(false);
+                radioButtonTcp.setChecked(false);
+                radioButtonMulticast.setChecked(false);
+            }
+        });
+
+        radioButtonMulticast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //stop button press
+                Log.v("radioButtonMulticast", "radio Button Multicast");
+                radioButtonUdp.setChecked(false);
+                radioButtonTcp.setChecked(false);
+                radioButtonUnicast.setChecked(false);
+            }
         });
     }
 
     public void play(View view) {
+        Log.v("play_button", "play button press");
 
         Thread d = new Thread(new Runnable() {  // asynchronously save image to parse
             @Override
@@ -76,7 +163,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         d.start();
-
 
     }
 
