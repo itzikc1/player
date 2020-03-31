7869fa72e05e68acc42f498c362eeed6bf12af2d
itamarlevit <itamar.levit@gmail.com>
2020-03-31 11:36:50 +0300

    remove comments
    remove comments

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -31,33 +31,20 @@ unsigned int Socket_Str2Addr(char *pc_ip){
 
 static void speedlive_user_overlay_callback(void *param,int video_ch,unsigned int pixelformat,int witdh,int height,int pitch ,void *bitmap,void *ext){
     static int frame=0;
-//    unsigned int *pdest = ( unsigned int  *)window_buffer.bits;
+
     int result;
     if(first_time) {
         result = ANativeWindow_setBuffersGeometry(native_window, witdh, height, WINDOW_FORMAT_RGBX_8888);
         first_time =0;
     }
 
-//    unsigned char *psrc_frame  = (unsigned char *)bitmap;
-//    int y,x;
-//
+
      result = ANativeWindow_lock(native_window, &window_buffer, NULL);
     unsigned int *pdest = ( unsigned int  *)window_buffer.bits;
     memcpy(pdest,bitmap,pitch*height);
-   // memset(pdest,0x255,witdh*height);
-//    for(y=0;y<576;y++){
-//        unsigned char *p_src_line= psrc_frame;
-//        for(x=0;x<720;x++){
-//           memcpy((void*)pdest,(void*)p_src_line,3);
-//           pdest += 1;
-//           p_src_line +=3;
-//        }
-//        psrc_frame += pitch;
-//    }
-
 
    ANativeWindow_unlockAndPost(native_window);
-   // __android_log_print(ANDROID_LOG_INFO, "sometag", "test int = %d", frame++);
+
 }
 
 static int start_streaming_rtsp(void* m_VidHadle , char *url,char *sdp,int protocol){
@@ -127,39 +114,15 @@ Java_com_example_player1_Player_playVideo(JNIEnv *env, jobject thiz, jobject sur
      m_speedlive = speedlive_get_handle();
     if(m_speedlive) {
         m_speedlive->Speedlive_get_dll_ver(&version);
-//        fprintf(stderr,"%s.%d version(%d)\n\r",__func__,__LINE__,version);
 
-//        LOGE("Player Error : Can not set native window buffer");
     }
-//
-//
-//
-//    if (result < 0){
-////        LOGE("Player Error : Can not set native window buffer");
-//        ANativeWindow_release(native_window);
-//        return;
-//    }
-//
-//    unsigned char *pbits = ( unsigned char *)window_buffer.bits;
-//
+
        result = m_speedlive->Speedlive_open(setup,(speedlive_handle_t *)(&m_VidDisp1));
-//        __android_log_print(ANDROID_LOG_INFO, "player", "test int = %d , handl(%d) results(%d)", version,m_VidDisp1,result);
+
        m_speedlive->Speedlive_register_user_overlay((speedlive_handle_t )m_VidDisp1, speedlive_user_overlay_callback,0);
-       result  = start_streaming(m_VidDisp1,Protocol_UDPTS,"",1234);
+       //result  = start_streaming(m_VidDisp1,Protocol_Split,"",1234);
+      result  = start_streaming(m_VidDisp1,Protocol_UDPTS,"",1234);
        //result = start_streaming_rtsp(m_VidDisp1,"192.168.0.44","mux1.sdp",0);
-//
-//    /*
-//    for(int y=0;y<800;y++){
-//        for(int x=0;x<1280;x++){
-//            pbits[0] = 255;
-//            pbits[1] = 0;
-//            pbits[2] = 0;
-//            pbits[3] = 0;
-//            pbits += 4;
-//        }
-//    }
-//    */
-//
-    
+
 
 }
